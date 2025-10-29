25f9bf2722a5c53cff2e8b8988746862a73d145f
Scarlet1107 <shougo.ushiro@gmail.com>
2025-10-29 15:21:13 +0900

    fix: build err

--- vitest.config.ts
+++ vitest.config.ts
@@ -5,7 +5,6 @@ export default defineConfig({
     test: {
         environment: "node",
         include: ["tests/**/*.test.ts"],
-        threads: false,
         pool: "forks",
         isolate: false,
         coverage: {
